43d4cb335f308aa026e2757e9dac49610f0f1670
nukloca <nicobelfio17@gmail.com>
2025-09-02 20:01:55 -0300

    Creacion MVC

---
+++ INSTITUTO_C/INSTITUTO_C/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace INSTITUTO_C.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
