a4390e0a9866dfd1f875731230c10b83b1b71ceb
OrtDehnikeEzequiel <36315016@comunidadort.edu.ar>
2025-09-02 22:33:52 -0300

    Propiedades en clase Persona y Profesor

--- INSTITUTO_C/INSTITUTO_C/Models/Persona.cs
+++ INSTITUTO_C/INSTITUTO_C/Models/Persona.cs
@@ -1,6 +1,38 @@
-﻿namespace INSTITUTO_C.Models
+﻿using System;
+using System.Collections.Generic;
+
+namespace INSTITUTO_C.Models
 {
     public class Persona
     {
+        //- UserName
+        //- Email
+        //- FechaAlta
+        //- Nombre
+        //- Apellido
+        //- DNI
+        //- Telefono
+        //- Direccion
+        //- Activo
+
+        public int id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime FechaAlta { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public string DNI { get; set; }
+
+        public int Telefono { get; set; }
+
+        public string Direccion { get; set; }
+
+        public bool Activo { get; set; }
     }
 }
--- INSTITUTO_C/INSTITUTO_C/Models/Profesor.cs
+++ INSTITUTO_C/INSTITUTO_C/Models/Profesor.cs
@@ -1,6 +1,48 @@
-﻿namespace INSTITUTO_C.Models
+﻿using System;
+using System.Collections.Generic;
+
+namespace INSTITUTO_C.Models
 {
     public class Profesor
     {
+        //- UserName
+        //- Email
+        //- FechaAlta
+        //- Nombre
+        //- Apellido
+        //- DNI
+        //- Telefono
+        //- Direccion
+        //- Activo
+        //- Legajo
+        //- MateriasCursada(Designado)
+        //- Calificaciones(Realizadas)
+
+        public int id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime FechaAlta { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public string DNI { get; set; }
+
+        public int Telefono { get; set; }
+
+        public string Direccion { get; set; }
+
+        public bool Activo { get; set; }
+
+        public string Legajo { get; set; }
+
+        public List<MateriaCursada> MateriasCursada { get; set; }
+
+        public List<Calificacion> Calificaciones { get; set; }
+
     }
 }
