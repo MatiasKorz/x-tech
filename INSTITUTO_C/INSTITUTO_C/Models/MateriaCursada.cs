3a16e22c83540b65fa4a8af09d6277ff9fc9a3ac
nbelfioreORT <43820677@comunidadort.edu.ar>
2025-09-16 22:24:05 -0300

    Actualizacion MateriaCursada.cs

--- INSTITUTO_C/INSTITUTO_C/Models/MateriaCursada.cs
+++ INSTITUTO_C/INSTITUTO_C/Models/MateriaCursada.cs
@@ -1,18 +1,42 @@
-﻿using System;
+﻿using INSTITUTO_C.Helpers;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace INSTITUTO_C.Models
 {
+    public enum Cuatrimestre
+    {
+        Primero = 1, // 1C
+        Segundo = 2  // 2C
+    }
+
     public class MateriaCursada
     {
         public int Id { get; set; }
+
+
+        [Required(ErrorMessage = ErrorMesseges.Requerido)]
+        public int MateriaId { get; set; }
         public Materia Materia { get; set; }
-        public char CodigoCursada { get; set; }
+
+        [Required(ErrorMessage = ErrorMesseges.Requerido)]
+        [StringLength(1, MinimumLength = 1, ErrorMessage = ErrorMesseges.CaracteresExactos)]
+        [RegularExpression("^[A-Z]$", ErrorMessage = ErrorMesseges.SoloLetras)]
+        public string CodigoCursada { get; set; }
+
+        [Required(ErrorMessage = ErrorMesseges.Requerido)]
         public int Anio { get; set; }
-        public int Cuatrimestre { get; set; }
-        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = ErrorMesseges.Requerido)]
+        public Cuatrimestre Cuatrimestre { get; set; }
+
         public bool Activo { get; set; }
+
         public Profesor Profesor { get; set; }
+
         public List<Inscripcion> Inscripciones { get; set; }
+
     }
 }
