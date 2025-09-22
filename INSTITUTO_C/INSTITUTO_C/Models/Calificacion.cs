21cbaaa508599c93b2fcb627de2d5669d717fa4b
matiaskorz <41562229@comunidadort.edu.ar>
2025-09-22 20:46:00 -0300

    Calificacion Incompleto

--- INSTITUTO_C/INSTITUTO_C/Models/Calificacion.cs
+++ INSTITUTO_C/INSTITUTO_C/Models/Calificacion.cs
@@ -1,6 +1,42 @@
-﻿namespace INSTITUTO_C.Models
+﻿using INSTITUTO_C.Helpers;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace INSTITUTO_C.Models
 {
     public class Calificacion
     {
+        //- Fecha(solo fecha)
+        //- Nota(enum)
+        //- Profesor
+        //- Inscripcion
+        //- Alumno
+
+        public int id { get; set; }
+
+
+
+
+
+        [Required(ErrorMessage = ErrorMesseges.Requerido)]
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy/MM/dd}")]
+        public DateTime FechaAlta { get; set; }
+
+
+        //????
+        public enum Nota { }
+        //????
+
+
+        public int ProfesorId { get; set; }
+        public Profesor Profesor { get; set; }
+
+        //????
+        public Inscripcion Inscripcion { get; set; }
+        //????
+
+        public int AlumnoId { get; set; }
+        public Alumno Alumno { get; set; }
     }
 }
