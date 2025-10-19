8e0d48683de2eaf86785e9fb13e76516cdaf3223
matiaskorz <41562229@comunidadort.edu.ar>
2025-10-18 21:33:49 -0300

    El enum de calificacion funciona correctamente, a diferencia de todo lo demas

---
+++ INSTITUTO_C/INSTITUTO_C/Models/Nota.cs
@@ -0,0 +1,23 @@
+﻿namespace INSTITUTO_C.Models
+{
+    public enum Nota
+    {
+
+     
+        Cero = 0,
+        Uno = 1,
+        Dos = 2,
+        Tres = 3,
+        Cuatro = 4,
+        Cinco = 5,
+        Seis = 6,
+        Siete = 7,
+        Ocho = 8,
+        Nueve = 9,
+        Diez = 10,
+        Ausente,
+        Pendiente,
+        Baja
+    }
+}
+
