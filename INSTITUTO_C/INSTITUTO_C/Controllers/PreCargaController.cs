07bb6a5ff0a2515f4b654b8fc91fb8be29f5212c
matiaskorz <41562229@comunidadort.edu.ar>
2025-10-27 13:00:18 -0300

    Pre Carga Basica de Roles

---
+++ INSTITUTO_C/INSTITUTO_C/Controllers/PreCargaController.cs
@@ -0,0 +1,48 @@
+﻿using INSTITUTO_C.Data;
+using INSTITUTO_C.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace INSTITUTO_C.Controllers
+{
+    public class PreCargaController : Controller
+    {
+
+        private readonly UserManager<Persona> _userManager;
+        private readonly RoleManager<IdentityRole<int>> _roleManager;
+        private readonly InstitutoContext _context;
+        private List<string> roles = new List<string>() {"Admin", "Empleado", "Profesor", "Alumno"};
+        public PreCargaController(UserManager<Persona> userManager, RoleManager<IdentityRole<int>> roleManager, InstitutoContext context)
+        {
+            this._userManager = userManager;
+            this._roleManager = roleManager; 
+            this._context = context;
+
+        }
+
+
+
+        public IActionResult Seed()
+        {
+            CrearRoles().Wait();
+
+
+
+            return RedirectToAction("Index","Home");
+        }
+
+        private async Task CrearRoles()
+        {
+            foreach (var rolName in roles)
+            {
+                if (!await _roleManager.RoleExistsAsync(rolName))
+                {
+                   await _roleManager.CreateAsync(new IdentityRole<int> { Name= rolName});
+                }
+            }
+        }
+    }
+}
