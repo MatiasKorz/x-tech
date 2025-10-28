cf79ee36855d6486e9298d57ad9e51344f34576a
OrtDehnikeEzequiel <36315016@comunidadort.edu.ar>
2025-10-28 07:41:49 -0300

    Modificacion en el home de info y estilos personalizados

---
+++ INSTITUTO_C/INSTITUTO_C/Migrations/20251028041537_Sqldb.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace INSTITUTO_C.Migrations
+{
+    /// <inheritdoc />
+    public partial class Sqldb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
